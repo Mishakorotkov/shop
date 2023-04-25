a3f87f94f4743b273f983f776aa8557b8aae42e3
Mishakorotkov <korotkov.misha1@yandex.ru>
2023-04-25 17:19:54 +0700

    jjson 4

--- inter_shop/shop.js
+++ inter_shop/shop.js
@@ -401,9 +401,10 @@ app.post('/admin/add', urlencodedParser, function(req, res) {
 
 
 app.get('/', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify([1, 2, 3, 4]));
 });
-
+//https://ru.stackoverflow.com/questions/483877/%D0%9A%D0%B0%D0%BA-%D0%B2-express-4-x-%D0%BF%D1%80%D0%B8%D0%BD%D1%8F%D1%82%D1%8C-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5-application-json
 
 
 app.listen(3000, function() {
